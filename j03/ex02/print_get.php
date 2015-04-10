983e576a5c9b0ef4a33f1d311a6e5b7a726d23d6
Valentin AME <vame@e2r4p8.42.fr>
2015-04-10 17:36:52 +0200

    j03 fini

--- j03/ex02/print_get.php
+++ j03/ex02/print_get.php
@@ -1,14 +1,7 @@
 <?php
 
-if ($_GET) {
-	$i = 0;
-	$keys = array_keys($_GET);
-	$len = count($keys);
-
-	while ($i < $len) {
-		$key = $keys[$i++];
-		echo $key.": ".$_GET[$key]."\n";
-	}
+foreach ($_GET as $key => $value) {
+	echo $key.": ".$value."\n";
 }
 
 ?>
